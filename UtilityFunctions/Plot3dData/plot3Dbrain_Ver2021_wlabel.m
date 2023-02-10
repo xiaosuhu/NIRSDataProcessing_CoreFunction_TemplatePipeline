c6d4cdcf337c0c1eeba33ad2a553adbf1f438195
Frank HU <xiaosuhu@outlook.com>
2023-02-10 16:57:00 -0500

    minor updates

---
+++ UtilityFunctions/Plot3dData/plot3Dbrain_Ver2021_wlabel.m
@@ -0,0 +1,46 @@
+function plot3Dbrain_Ver2021_wlabel(intensity,onlypositive,p,coordfile)
+
+coord=load(coordfile,'-mat'); % Load Coordinates - now need to specify names stroing the data
+fieldname=fields(coord);
+CHMNI=eval(['coord.',fieldname{1}]);
+
+mx=4;
+mn=-4;
+
+% remove the negative intensity associated ind
+if onlypositive
+    negind=find(intensity<=0);
+else
+    negind=[];
+end
+
+insigind=find(p>=.99);
+
+if ~isempty(negind)
+    try
+        rind=unique([negind; insigind]);
+    catch
+        rind=unique([negind insigind]);
+    end
+else
+    rind=insigind;
+end
+
+intensity(rind)=[];
+CHMNI(rind,:)=[];
+
+CHMNIcoordstd=10*ones(length(CHMNI));
+
+Plot3D_channel_registration_result_Ver2021(intensity, CHMNI, CHMNIcoordstd,mx,mn);
+
+x_translate=96.5;
+y_translate=114.5+5;
+z_translate=96.5-10;
+
+MNIcoord=CHMNI+repmat([x_translate y_translate z_translate],size(CHMNI,1),1);
+
+for i=1:length(MNIcoord)
+    text(MNIcoord(i,1)-5,MNIcoord(i,2),MNIcoord(i,3),num2str(i));
+end
+
+end
