e18031133fbe2613b634b492fc9af09c05c85809
Frank HU <xiaosuhu@outlook.com>
2025-02-08 21:04:39 -0500

    Update: check all the code and pipeline

--- AnalyticPipeline/FirstLevelGLM/fNIRS_FirstLevelGLM.m
+++ AnalyticPipeline/FirstLevelGLM/fNIRS_FirstLevelGLM.m
@@ -1,96 +1,95 @@
-%% fNIRS Data Analyses - Bilingual Litercy Project 
-%% Morphosyntax Grammaticality Judgement Task
-% Script 2 of 4
-% 
-% Neelima Wagley
-% 
-% Modified: 08/07/18
-% 
-% Xiaosu Hu
-% 
-% Modified:Apr.29.2019
+%% fNIRS Data Analyses general pipeline part 1 - preprocess and first level glm
+% Created by Xiaosu Frank Hu 2/8/2025
+
 %% Preprocessing & GLM First (individual) Level Analyses
 
 % Data Analyses Pipile as listed below using Huppert et al NIRS Toolbox and
 % inidividualized scripts created by Frank Hu.
 
 % 2. Preprocessing
+%    - Add SS channel
 %    - Resample data at lower frequency
-%    - ADD MORE DETAILS HERE
+%    - MBLL to Hb data
+%    - Trim the data section not needed for first level GLM
 % 3. Subject (1st) Level GLM
-%% Add path to NIRS toolbox (Huppert et al.)
-% Insert hyperlink to NIRS Toolbox here. 
-%%
-% toolboxdir=uigetdir();
-% addpath(genpath(toolboxdir));
+% 4. Demographic variable add
+
 %% Define data directory and load raw rataset
-%%
 clear
-datadir =uigetdir();
-% Gain adjustment
-% GainAdjustmentforCW6(datadir)
-%load Data
-raw = nirs.io.loadDirectory(datadir, {'Subject'});
+% use this line to select data dir by GUI
+datadir = uigetdir(); 
+% or use this line to specify your data dir directly
+% datadir = 'PATH TO YOUR DATA DIR'; 
 
-%% Check the data quality
-% digit1=77;
-% digit2=97;
-% chinterest=1;
-% CWTCardiacBaselineShiftCheck(raw,chinterest,digit1,digit2);
+%load Data with different levels, the group task and subjet will be used as
+%demographics
+% rawdata = nirs.io.loadDirectory(datadir, {'Group', 'Task', 'Subject'});
+% rawdata = nirs.io.loadDirectory(datadir, {'Group','Subject'});
+rawdata = nirs.io.loadDirectory(datadir, {'Subject'});
 
-%% Check HbO HbR Correlation
-% hbohbrcorr=HbOHbRCorrCheck(raw);
-
-%% Mark stimuli onsets for analyses 
-%%
-% OPTIONAL: CAN SHIFT TIMECOURSE ONSET OF STIMS
-
-onset_shift = 0; %duration of onset time SHIFT (sec)
-dur = 8; %duration of trial (sec)
-
-for i=1:length(raw)
-    for j=1:length(raw(i).stimulus.keys)
-        raw(i).stimulus.values{j}.onset=raw(i).stimulus.values{j}.onset + repmat(onset_shift,size(raw(i).stimulus.values{j}.onset,1),1);
-        raw(i).stimulus.values{j}.dur=repmat(dur,size(raw(i).stimulus.values{j}.dur,1),1);
-    end
-end
-%% ADD CODE HERE TO PLOT RAW SIGNAL 
 %% Initialize GLM (Subject-Level) 
+% label short separation channels if you use SS in your data collection
+disp('Adding SS to the pipeline...')
+j_ss = nirs.modules.LabelShortSeperation();
+rawdata = j_ss.run(rawdata);
 
-firstlevelpipeline = nirs.modules.default_modules.single_subject;
-List = nirs.modules.pipelineToList(firstlevelpipeline); % Applies job
+disp('Running data resample...')
+resample=nirs.modules.Resample();
+resample.Fs=2; % Resample the data to 2 Hz
+downraw=resample.run(rawdata);
 
-% Downsample Data from 50Hz to N Hz
-List{4}.Fs = 2;
+disp('Converting Optical Density...')
+odconv=nirs.modules.OpticalDensity();
+od=odconv.run(downraw);
 
-List{8}.preBaseline=5;
-List{8}.postBaseline=5;
+disp('Applying  Modified Beer Lambert Law...')
+mbll=nirs.modules.BeerLambertLaw();
+hb=mbll.run(od);
 
-% GLM Basis Function
+disp('Trimming .nirs files...')
+trim=nirs.modules.TrimBaseline();
+trim.preBaseline=5; % 5 sec before the first stim
+trim.postBaseline=5; % 5 sec after the last stim
+hb_trim=trim.run(hb);
+
+% save('Data_preprocessed_hb.mat','hb_trim','-v7.3','-nocompression')
+
+%% First level analysis
+disp('Now running subject-level GLM!')
+firstlevelglm=nirs.modules.GLM();
+
+firstlevelglm.type = 'AR-IRLS';
+firstlevelglm.AddShortSepRegressors = true; % SS channel set up
+
 firstlevelbasis = nirs.design.basis.Canonical();
-firstlevelbasis.incDeriv=0;
+% Adding temporal & dispersion derivatives to canonical HRF function
+% firstlevelbasis.incDeriv=1;
+% DCT matrix to account for signal drift over time
+% firstlevelglm.trend_func=@(t) nirs.design.trend.dctmtx(t,0.008);
+% HRF peak time = 6s based on Friederici and Booth papers (e.g. Brauer, Neumann & Friederici, 2008, NeuroImage)
+firstlevelbasis.peakTime = 6;
 
-% Hemodynamic Response Peak Time
-firstlevelbasis.peakTime = 6; % peak time determined based on Friederici and Booth papers (insert link)
-List{9}.basis('default') = firstlevelbasis;
-List{9}.trend_func = @(t) nirs.design.trend.dctmtx(t,0.005);
+% Run
+firstlevelglm.basis('default') = firstlevelbasis;
+SubjStats=firstlevelglm.run(hb_trim);
+disp('Ready to save SubjStats...')
 
-% Convert the modified list back to pipeline
-firstlevelpipeline = nirs.modules.listToPipeline(List);
-%% Complete First Level Pre-Processing & GLM
-%%
-% Run Job
-firstlevelpipeline.run(raw);
-%% Initialize GLM group level analyses (mixed effects)
-% Initalize GLM
-grouplevelpipeline = nirs.modules.MixedEffects();
+save('SubjStats.mat','SubjStats','-v7.3','-nocompression')
+disp('Done!')
 
-% Run GML with SEPARATE conditions and NO REGRESSORS. Only TASK (conditions) vs. REST
+%% Add Age & Gender for demographic as example
+% the path to a csv file containing demographics
 
-grouplevelpipeline.formula ='beta ~ -1 + cond + (1|Subject)';
-GroupStats = grouplevelpipeline.run(SubjStats);
+% Method 1
+% Demo1 = nirs.modules.AddDemographics();
+% Demo1.demoTable = readtable('F:\MatlabBackUp\PROJECT_HH_R01\TOM_modeling_behavior/LangLit Data 2023.xlsx','Sheet', 'Behavioral');
+% Demo1.varToMatch='Subject';
+% SubjStats = Demo1.run(SubjStats);
 
-GroupStats.draw('tstat',[-4 4],'p<.05');
+% Method 2
+% Grouping = readtable('./Behave.csv');
+% Demo = nirs.modules.AddDemographics();
+% Demo.demoTable = Grouping;
+% Demo.varToMatch='Subject';
+% SubjStats = Demo.run(SubjStats);
 
-c=[1 -0.5 -0.5];
-Contrast = GroupStats.ttest(c);
---
+++ AnalyticPipeline/GroupLevelGLM/fNIRS_GroupLevelGLM_reference.m
@@ -0,0 +1,54 @@
+%% fNIRS Data Analyses general pipeline part 2 - group level linear mixed effects model
+% Created by Xiaosu Frank Hu 2/8/2025
+
+%% Second (group) Level Analyses
+
+% Data Analyses Pipile as listed below using Huppert et al NIRS Toolbox and
+% inidividualized scripts created by Frank Hu.
+
+% 1. Group Level (2nd) GLM (mixed effects)
+% 2. Group Contrasts & Statistics
+% 3. ANOVA
+
+%% GLM group level analyses (mixed effects) - dummy coding = reference
+% Initalize GLM
+grouplevelpipeline = nirs.modules.MixedEffects();
+% to use robust regression also for the group level, but will take longer time
+% grouplevelpipeline.robust=true; 
+grouplevelpipeline.dummycoding = 'reference';
+
+% ANOVA in LME way
+grouplevelpipeline.formula ='beta ~ group*cond + age + (1|Subject)';
+
+% add controlling variable for the slope
+% grouplevelpipeline.formula ='beta ~ -1 + group*task*cond + (age|Subject)';
+
+GroupStats= grouplevelpipeline.run(SubjStats);
+% draw the results
+% GroupStats.draw('tstat',[-4 4]);
+
+%% Contrast 
+
+c = [1 0 0]; % contrast vector
+Contrast = GroupStats.ttest(c);
+Contrast_table = Contrast.table; % convert the contrast results to a table
+
+
+%% Plot on 3D brain for hbo result
+beta_oi = Contrast_table.tstat(1:2:end-1);
+p_oi = Contrast_table.q(1:2:end-1);
+
+intensity = beta_oi;
+p=p_oi;
+onlypositive = 1;
+
+figure('Color',[1 1 1])
+
+subplot(1,2,1)
+plot3Dbrain_Ver2021(intensity,onlypositive,p,'Orig_32.mat') % Orig_32.mat will be a matrix containing the MNI coordinates
+
+subplot(1,2,2)
+plot3Dbrain_Ver2021(intensity,onlypositive,p,'Orig_32.mat')
+view(90,0)
+
+
